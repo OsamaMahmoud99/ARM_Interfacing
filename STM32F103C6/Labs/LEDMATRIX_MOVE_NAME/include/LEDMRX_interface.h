e5d1f6ed607b5a76f479a8005e26c05cd2a1f2d1
osama <gad153879@gmail.com>
2023-05-13 20:19:31 +0200

    MoveName

---
+++ STM32F103C6/Labs/LEDMATRIX_MOVE_NAME/include/LEDMRX_interface.h
@@ -0,0 +1,13 @@
+#ifndef LEDMRX_INTERFACE_H
+#define LEDMRX_INTERFACE_H
+
+
+
+void LEDMRX_voidDispaly(u8 *Copy_u8Data);
+void LEDMRX_voidInit(void);
+void LEDMRX_voidDisplayName(u8 *Copy_u8Data);
+void LEDMRX_voidDisplayFullName(u8 *Copy_u8Data);
+
+
+
+#endif
